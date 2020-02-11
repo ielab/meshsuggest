dae3d81b8b621a0c8873b691ab2a0faf437e59c5
Hang Li <cecillll.lee@gmail.com>
2020-02-11 23:09:13 +1000

    add atm experiment code

--- helper.py
+++ helper.py
@@ -1,14 +1,105 @@
-from bs4 import BeautifulSoup
+import shutil
+
 import requests
+import json
+
+symbols = ['*', '"', '[MeSH Terms]']
 
 
-def readFile(file):
-    content = file.read()
-    print(content)
-    requestForSearchDetails(content)
+def readFile(mode, file):
+    if mode is "c":
+        queryContent = file.read()
+        # lineSeperator("-")
+        # print("Sub-Clause Content: " + queryContent)
+        generatedMesh = requestForSearchDetails(queryContent)
+        return generatedMesh
+    else:
+        meshContent = file.read()
+        meshs = meshContent.split("\n")
+        cleanedMeshs = cleanTerms(meshs)
+        # lineSeperator("-")
+        # print("Original MeSH Terms: ")
+        # for mesh in cleanedMeshs:
+        #     print(mesh)
+        return cleanedMeshs
 
 
 def requestForSearchDetails(query):
-    url = "https://www.ncbi.nlm.nih.gov/pubmed/?term=" + query
+    url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&term=" + query
     response = requests.get(url, params=None)
-    print(response)
+    res = json.loads(response.content)
+    translationStack = res["esearchresult"]["translationstack"]
+    # generatedQuery = res["esearchresult"]["querytranslation"]
+    generatedMesh = getATMMeSHTerms(translationStack)
+    # print("Generated Query: " + generatedQuery)
+    # lineSeperator("-")
+    # print("Generated MeSH Terms: ")
+    # for mesh in generatedMesh:
+    #     print(mesh)
+    return generatedMesh
+
+
+def cleanTerms(bucket):
+    res = []
+    for item in bucket:
+        for char in symbols:
+            item = item.replace(char, "")
+        res.append(item.lower())
+    res = list(dict.fromkeys(res))
+    return res
+
+
+def getATMMeSHTerms(translationstack):
+    mesh = []
+    for item in translationstack:
+        if type(item) is not str and item["field"] == "MeSH Terms":
+            mesh.append(item["term"])
+    res = cleanTerms(mesh)
+    return res
+
+
+def findMatch(original, generated):
+    hitMesh = set(original) & set(generated)
+    return hitMesh
+
+
+def writeFile(path, filename, data):
+    f = open(path + "/" + filename, "w+")
+    f.write(data)
+    return
+
+
+# From https://gist.github.com/greenstick/b23e475d2bfdc3a82e34eaa1f6781ee4
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', autosize=False):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        autosize    - Optional  : automatically resize the length of the progress bar to the terminal window (Bool)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    styling = '%s |%s| %s%% %s' % (prefix, fill, percent, suffix)
+    if autosize:
+        cols, _ = shutil.get_terminal_size(fallback=(length, 1))
+        length = cols - len(styling)
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s' % styling.replace(fill, bar), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+def lineSeperator(fill='#', length=100):
+    styling = '%s' % fill
+    cols, _ = shutil.get_terminal_size(fallback=(length, 1))
+    length = cols - len(styling)
+    bar = fill * length
+    print('\r%s' % styling.replace(fill, bar), end='\r')
+    print()
--- main.py
+++ main.py
@@ -23,26 +23,46 @@ TOTAL_FOLDER_2019 = "data/clef_tar_processed/2019/total"
 # TOTAL DATASET
 TOTAL_DATASET = "data/clef_tar_processed/total"
 
+PATHS = [TEST_FOLDER_2017, TRAIN_FOLDER_2017, TEST_FOLDER_2018, TRAIN_FOLDER_2018, TEST_DTA_FOLDER_2019,
+         TEST_INTERVENTION_FOLDER_2019, TOTAL_TEST_FOLDER_2019, TRAIN_DTA_FOLDER_2019, TRAIN_INTERVENTION_FOLDER_2019,
+         TOTAL_TRAIN_FOLDER_2019, TOTAL_FOLDER_2017, TOTAL_FOLDER_2018, TOTAL_FOLDER_2019]
+
 
 def main():
-    paths = [TOTAL_FOLDER_2017, TOTAL_FOLDER_2018, TOTAL_FOLDER_2019]
-    for path in paths:
-        print(path)
+    for path in PATHS:
+        lineSeperator("#")
+        print("Dataset: " + path)
         dirs = os.listdir(path)
-        for d in dirs:
+        totalHits = 0
+        totalMeSHs = 0
+        printProgressBar(0, len(dirs), prefix='Progress', suffix='Complete', autosize=True)
+        for i, d in enumerate(dirs):
+            printProgressBar(i + 1, len(dirs), prefix='Progress', suffix='Complete', autosize=True)
             if d is not ".DS_Store" and os.path.isdir(path + "/" + d):
                 innerD = os.listdir(path + "/" + d)
                 if ".DS_Store" in innerD:
                     innerD.remove(".DS_Store")
                 for dd in innerD:
-                    print(d)
-                    print(dd)
-                    # mesh_f = open(path + "/" + d + "/" + dd + "/" + "mesh", "r")
-                    # print(path + "/" + d + "/" + dd + "/" + "mesh")
-                    # readFile(mesh_f)
+                    # lineSeperator("-")
+                    # print("Topic: " + d)
+                    # print("Sub-Clause Number: " + dd)
+                    # print("Original MeSH Path: " + path + "/" + d + "/" + dd + "/" + "mesh")
+                    # print("Query Path: " + path + "/" + d + "/" + dd + "/" + "clean_clause")
+                    mesh_f = open(path + "/" + d + "/" + dd + "/" + "mesh", "r")
                     clause_no_mesh_f = open(path + "/" + d + "/" + dd + "/" + "clean_clause", "r")
-                    print(path + "/" + d + "/" + dd + "/" + "clean_clause")
-                    readFile(clause_no_mesh_f)
+                    originalMesh = readFile("m", mesh_f)
+                    generatedMesh = readFile("c", clause_no_mesh_f)
+                    totalMeSHs += len(originalMesh)
+                    hits = findMatch(originalMesh, generatedMesh)
+                    totalHits += len(hits)
+                    # lineSeperator("-")
+                    # print("Number of Terms Match: " + str(len(hits)))
+                    # print("Correctness: " + str((round(len(hits) / len(originalMesh), 4)) * 100) + "%")
+        # lineSeperator("-")
+        print("Total Mesh Terms: " + str(totalMeSHs))
+        print("Total Matched Terms: " + str(totalHits))
+        print("Overall Correctness: " + str((round(totalHits / totalMeSHs, 4)) * 100) + "%")
+        lineSeperator("#")
 
 
 if __name__ == "__main__":
