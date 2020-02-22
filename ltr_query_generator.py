06e49ef77aa5c6c4d3c7c0dd36ff183ab673d12f
Hang Li <cecillll.lee@gmail.com>
2020-02-23 02:11:00 +1000

    finish method to generate query file based on the ltr res files

--- ltr_query_generator.py
+++ ltr_query_generator.py
@@ -1,5 +1,13 @@
 import os
 import re
+import json
+import collections
+
+MESHINFOF = open("mesh.json", "r")
+MESHINFO = json.loads(MESHINFOF.read())
+MESHINFOF.close()
+
+PATH_PREFIX = "/Users/summerfrogman/ielab/meshsuggest/data/clef_tar_processed"
 
 
 def main():
@@ -7,9 +15,95 @@ def main():
     trash = ".DS_Store"
     if trash in filenames:
         filenames.remove(trash)
-    for filename in filenames:
-        grouped = readAndParseResFile(filename)
-        print(grouped)
+    # for filename in filenames:
+    grouped = readAndParseResFile(filenames[0])
+    print(filenames[0])
+    desc = grouped[0][0]["desc"]
+    splitedDesc = desc.split("_")
+    year = splitedDesc[0]
+    method = splitedDesc[1].lower()
+    originalDataPath = "{}/{}/testing".format(PATH_PREFIX, year)
+    completeTopics = getCompleteTopicList(originalDataPath)
+    ltrTopicAndMesh = getLTRTopicsAndMeSH(grouped)
+    ltrTopicAndMesh = compareAndAddMissingTopic(originalDataPath, completeTopics, ltrTopicAndMesh)
+    ltrTopicAndMesh = compareAndAddMissingSub(originalDataPath, ltrTopicAndMesh)
+    produceLTRQuery(ltrTopicAndMesh, method, originalDataPath)
+
+
+def produceLTRQuery(ltrTopics, method, path):
+    topic = list(ltrTopics.keys())
+    for t in topic:
+        query = constructLTRQuery(t, ltrTopics, path)
+        writeQueryFile(query, t, path, method)
+
+
+def writeQueryFile(query, t, path, method):
+    queryOutPath = "{}/{}/ltr_{}_result_query".format(path, t, method.lower())
+    f = open(queryOutPath, "w+")
+    f.write(query)
+    f.close()
+
+
+def constructLTRQuery(topic, ltrTopics, path):
+    allSubs = ltrTopics[topic]
+    allSubKeys = []
+    for each in allSubs:
+        allSubKeys += list(each.keys())
+    queryFragmentsList = []
+    for key in allSubKeys:
+        newPath = "{}/{}/{}".format(path, topic, key)
+        cleanQuery = getCleanQuery(newPath)
+        if len(allSubs[int(key)-1][key]) > 0:
+            meshTerms = allSubs[int(key)-1][key]
+            meshQuery = "[mesh] OR ".join(meshTerms)
+            newQuery = "(" + meshQuery + "[mesh] OR " + cleanQuery[1:]
+            queryFragmentsList.append(newQuery)
+        else:
+            queryFragmentsList.append(cleanQuery)
+    return " AND ".join(queryFragmentsList)
+
+
+def compareAndAddMissingSub(originalDataPath, ltrTopics):
+    topics = list(ltrTopics.keys())
+    for each in topics:
+        subs = getCompleteTopicList("{}/{}".format(originalDataPath, each))
+        ltrSubsList = ltrTopics[each]
+        ltrSubs = []
+        for item in ltrSubsList:
+            ltrSubs += item.keys()
+        diff = list(set(subs) - set(ltrSubs))
+        if len(diff) > 0:
+            temp = {}
+            for sub in diff:
+                temp = {
+                    sub: []
+                }
+            ltrTopics[each].append(temp)
+    for k in topics:
+        ltrTopics[k].sort(key=getKey)
+    return ltrTopics
+
+
+def getKey(item):
+    keys = list(item.keys())
+    key = keys[0]
+    return key
+
+
+def compareAndAddMissingTopic(originalDataPath, completeTopics, ltrTopics):
+    diff = list(set(completeTopics) - set(ltrTopics.keys()))
+    if len(diff) > 0:
+        for each in diff:
+            subs = getCompleteTopicList("{}/{}".format(originalDataPath, each))
+            subList = []
+            for s in subs:
+                temp = {
+                    s: []
+                }
+                subList.append(temp)
+            ltrTopics[each] = subList
+    orderedLTRTopics = collections.OrderedDict(sorted(ltrTopics.items()))
+    return orderedLTRTopics
 
 
 def readAndParseResFile(filename):
@@ -22,7 +116,8 @@ def readAndParseResFile(filename):
         splitedLine = re.split(r' +', each)
         oneObj = {
             "topic": splitedLine[0],
-            "uid": splitedLine[2]
+            "uid": splitedLine[2],
+            "desc": splitedLine[5]
         }
         allItems.append(oneObj)
     topics = []
@@ -43,6 +138,58 @@ def readAndParseResFile(filename):
     for g in groupedAll:
         g.sort(key=lambda x: x["uid"])
     return groupedAll
+
+
+def getCleanQuery(path):
+    newPath = "{}/clause_no_mesh".format(path)
+    f = open(newPath, "r")
+    cleanQuery = f.read()
+    return cleanQuery
+
+
+def getCompleteTopicList(path):
+    lst = [dI for dI in os.listdir(path) if os.path.isdir(os.path.join(path, dI))]
+    lst.sort()
+    return lst
+
+
+def getLTRTopicsAndMeSH(grouped):
+    ltrTopics = {}
+    tempTopics = []
+    for each in grouped:
+        topic = each[0]["topic"].split("_")[0]
+        tempTopics.append(topic)
+    uniqueTopics = list(dict.fromkeys(tempTopics))
+    for t in uniqueTopics:
+        temp = []
+        for ele in grouped:
+            if t in ele[0]["topic"]:
+                for i in ele:
+                    temp.append(i["topic"].split("_")[1])
+        temp = list(dict.fromkeys(temp))
+        ltrTopics[t] = temp
+    for top in uniqueTopics:
+        subList = ltrTopics[top]
+        meshSubObj = []
+        for eve in subList:
+            lowerTopic = "{}_{}".format(top, eve)
+            mesh = []
+            for i in grouped:
+                if i[0]["topic"] == lowerTopic:
+                    for k in i:
+                        mesh.append(getSubTopicMeSHTerms(k["uid"]))
+            topicMeshObj = {eve: mesh}
+            meshSubObj.append(topicMeshObj)
+        ltrTopics[top] = meshSubObj
+    return ltrTopics
+
+
+def getSubTopicMeSHTerms(uid):
+    mesh = None
+    for item in MESHINFO:
+        if item["uid"] == uid:
+            mesh = item["term"]
+    return mesh
 
 
 if __name__ == '__main__':
