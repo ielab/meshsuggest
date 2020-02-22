3047592c64da4997f0bf014af7bf8899537a13ef
Hang Li <cecillll.lee@gmail.com>
2020-02-22 18:37:06 +1000

    add cutoff training res files for ltr

---
+++ ltr_cutoff_res_files.py
@@ -0,0 +1,112 @@
+# Checked Test Res From LTR
+ATM_2017_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2017_ATM_test_ckd.res",
+                     "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2017_ATM_test_cutoff",
+                     "2017_ATM_test_cutoff"]
+
+UMLS_2017_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2017_UMLS_test_ckd.res",
+                      "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2017_UMLS_test_cutoff",
+                      "2017_UMLS_test_cutoff"]
+
+META_2017_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2017_Meta_test_ckd.res",
+                      "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2017_Meta_test_cutoff",
+                      "2017_Meta_test_cutoff"]
+
+ATM_2018_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2018_ATM_test_ckd.res",
+                     "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2018_ATM_test_cutoff",
+                     "2018_ATM_test_cutoff"]
+
+UMLS_2018_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2018_UMLS_test_ckd.res",
+                      "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2018_UMLS_test_cutoff",
+                      "2018_UMLS_test_cutoff"]
+
+META_2018_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2018_Meta_test_ckd.res",
+                      "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2018_Meta_test_cutoff",
+                      "2018_Meta_test_cutoff"]
+
+ATM_2019_D_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2019_ATM_D_test_ckd.res",
+                       "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2019_ATM_D_test_cutoff",
+                       "2019_ATM_D_test_cutoff"]
+
+UMLS_2019_D_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2019_UMLS_D_test_ckd.res",
+                        "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2019_UMLS_D_test_cutoff",
+                        "2019_UMLS_D_test_cutoff"]
+
+META_2019_D_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2019_Meta_D_test_ckd.res",
+                        "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2019_Meta_D_test_cutoff",
+                        "2019_Meta_D_test_cutoff"]
+
+ATM_2019_I_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2019_ATM_I_test_ckd.res",
+                       "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2019_ATM_I_test_cutoff",
+                       "2019_ATM_I_test_cutoff"]
+
+UMLS_2019_I_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2019_UMLS_I_test_ckd.res",
+                        "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2019_UMLS_I_test_cutoff",
+                        "2019_UMLS_I_test_cutoff"]
+
+META_2019_I_TEST_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/checked/2019_Meta_I_test_ckd.res",
+                        "/Users/summerfrogman/ielab/meshsuggest/ltr_res/test/cutoffs/2019_Meta_I_test_cutoff",
+                        "2019_Meta_I_test_cutoff"]
+
+# Checked Train Res From LTR
+ATM_2017_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2017_ATM_train_ckd.res",
+                      "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2017_ATM_train_cutoff",
+                      "2017_ATM_train_cutoff"]
+
+UMLS_2017_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2017_UMLS_train_ckd.res",
+                       "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2017_UMLS_train_cutoff",
+                       "2017_UMLS_train_cutoff"]
+
+META_2017_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2017_Meta_train_ckd.res",
+                       "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2017_Meta_train_cutoff",
+                       "2017_Meta_train_cutoff"]
+
+ATM_2018_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2018_ATM_train_ckd.res",
+                      "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2018_ATM_train_cutoff",
+                      "2018_ATM_train_cutoff"]
+
+UMLS_2018_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2018_UMLS_train_ckd.res",
+                       "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2018_UMLS_train_cutoff",
+                       "2018_UMLS_train_cutoff"]
+
+META_2018_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2018_Meta_train_ckd.res",
+                       "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2018_Meta_train_cutoff",
+                       "2018_Meta_train_cutoff"]
+
+ATM_2019_D_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2019_ATM_D_train_ckd.res",
+                        "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2019_ATM_D_train_cutoff",
+                        "2019_ATM_D_train_cutoff"]
+
+UMLS_2019_D_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2019_UMLS_D_train_ckd.res",
+                         "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2019_UMLS_D_train_cutoff",
+                         "2019_UMLS_D_train_cutoff"]
+
+META_2019_D_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2019_Meta_D_train_ckd.res",
+                         "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2019_Meta_D_train_cutoff",
+                         "2019_Meta_D_train_cutoff"]
+
+ATM_2019_I_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2019_ATM_I_train_ckd.res",
+                        "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2019_ATM_I_train_cutoff",
+                        "2019_ATM_I_train_cutoff"]
+
+UMLS_2019_I_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2019_UMLS_I_train_ckd.res",
+                         "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2019_UMLS_I_train_cutoff",
+                         "2019_UMLS_I_train_cutoff"]
+
+META_2019_I_TRAIN_RES = ["/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/checked/2019_Meta_I_train_ckd.res",
+                         "/Users/summerfrogman/ielab/meshsuggest/ltr_res/train/cutoffs/2019_Meta_I_train_cutoff",
+                         "2019_Meta_I_train_cutoff"]
+
+TRAIN_PATHS = [
+    ATM_2017_TRAIN_RES,
+    META_2017_TRAIN_RES,
+    UMLS_2017_TRAIN_RES,
+    ATM_2018_TRAIN_RES,
+    META_2018_TRAIN_RES,
+    UMLS_2018_TRAIN_RES,
+    ATM_2019_D_TRAIN_RES,
+    META_2019_D_TRAIN_RES,
+    UMLS_2019_D_TRAIN_RES,
+    ATM_2019_I_TRAIN_RES,
+    META_2019_I_TRAIN_RES,
+    UMLS_2019_I_TRAIN_RES
+]
--- ltr_res_cutoff.py
+++ ltr_res_cutoff.py
@@ -1,113 +1,93 @@
 import re
-
-# UMLS Train Res From LTR
-UMLS_2017_TRAIN_RES_PATH = "ltr_res/2017/train/2017_ltr_train_umls.res"
-UMLS_2018_TRAIN_RES_PATH = "ltr_res/2018/train/2018_ltr_train_umls.res"
-UMLS_2019_D_TRAIN_RES_PATH = "ltr_res/2019/train/DTA/2019_ltr_DTA_train_umls.res"
-UMLS_2019_I_TRAIN_RES_PATH = "ltr_res/2019/train/Intervention/2019_ltr_Intervention_train_umls.res"
-
-# ATM Train Res From LTR
-ATM_2017_TRAIN_RES_PATH = "ltr_res/2017/train/2017_ltr_train_atm.res"
-ATM_2018_TRAIN_RES_PATH = "ltr_res/2018/train/2018_ltr_train_atm.res"
-ATM_2019_D_TRAIN_RES_PATH = "ltr_res/2019/train/DTA/2019_ltr_DTA_train_atm.res"
-ATM_2019_I_TRAIN_RES_PATH = "ltr_res/2019/train/Intervention/2019_ltr_Intervention_train_atm.res"
-
-# METAMAP Train Res From LTR
-META_2017_TRAIN_RES_PATH = "ltr_res/2017/train/2017_ltr_train_meta.res"
-META_2018_TRAIN_RES_PATH = "ltr_res/2018/train/2018_ltr_train_meta.res"
-META_2019_D_TRAIN_RES_PATH = "ltr_res/2019/train/DTA/2019_ltr_DTA_train_meta.res"
-META_2019_I_TRAIN_RES_PATH = "ltr_res/2019/train/Intervention/2019_ltr_Intervention_train_meta.res"
-
-# UMLS Test Res From LTR
-UMLS_2017_TEST_RES_PATH = "ltr_res/2017/test/2017_ltr_test_umls.res"
-UMLS_2018_TEST_RES_PATH = "ltr_res/2018/test/2018_ltr_test_umls.res"
-UMLS_2019_D_TEST_RES_PATH = "ltr_res/2019/test/DTA/2019_ltr_DTA_test_umls.res"
-UMLS_2019_I_TEST_RES_PATH = "ltr_res/2019/test/Intervention/2019_ltr_Intervention_test_umls.res"
-
-# ATM Test Res From LTR
-ATM_2017_TEST_RES_PATH = "ltr_res/2017/test/2017_ltr_test_atm.res"
-ATM_2018_TEST_RES_PATH = "ltr_res/2018/test/2018_ltr_test_atm.res"
-ATM_2019_D_TEST_RES_PATH = "ltr_res/2019/test/DTA/2019_ltr_DTA_test_atm.res"
-ATM_2019_I_TEST_RES_PATH = "ltr_res/2019/test/Intervention/2019_ltr_Intervention_test_atm.res"
-
-# METAMAP Test Res From LTR
-META_2017_TEST_RES_PATH = "ltr_res/2017/test/2017_ltr_test_meta.res"
-META_2018_TEST_RES_PATH = "ltr_res/2018/test/2018_ltr_test_meta.res"
-META_2019_D_TEST_RES_PATH = "ltr_res/2019/test/DTA/2019_ltr_DTA_test_meta.res"
-META_2019_I_TEST_RES_PATH = "ltr_res/2019/test/Intervention/2019_ltr_Intervention_test_meta.res"
+from ltr_cutoff_res_files import *
 
 
 def main():
     nums = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-    # nums = []
-    path = UMLS_2017_TEST_RES_PATH
-    with open(path, "r") as f:
-        content = f.readlines()
-    content = [x.strip() for x in content]
-    allItems = []
-    for each in content:
-        splitedLine = re.split(r' +', each)
-        oneObj = {
-            "topic": splitedLine[0],
-            "uid": splitedLine[2],
-            "score": float(splitedLine[4])
-        }
-        allItems.append(oneObj)
-    topics = []
-    for item in allItems:
-        topics.append(item["topic"])
-    uniqueTopics = list(dict.fromkeys(topics))
-    uniqueTopics.sort()
-    groupedAll = []
-    for topic in uniqueTopics:
-        grouped = []
-        for t in allItems:
-            if t["topic"] == topic:
-                grouped.append(t)
-            else:
-                continue
-        if len(grouped) > 0:
-            groupedAll.append(grouped)
-    for g in groupedAll:
-        g.sort(key=lambda x: (x["score"], x["uid"]))
-    for each in groupedAll:
-        innerAllScores = []
-        for m in each:
-            innerAllScores.append(float(m["score"]))
-        innerMaxScore = max(innerAllScores)
-        for n in each:
-            n["score"] = innerMaxScore - n["score"]
-    for e in groupedAll:
-        e.sort(key=lambda y: (y["score"], y["uid"]))
-    cutoffGroupedAll = []
-    for num in nums:
-        num = float(num)
-        for c in groupedAll:
-            eachCutoffTotalScore = 0.0
-            for ele in c:
-                eachCutoffTotalScore += ele["score"]
-            if eachCutoffTotalScore == 0.0:
-                cutoffGroupedAll.append(c)
-            else:
-                eachCutoffScore = eachCutoffTotalScore * (num / 100.0)
-                tempScore = 0.0
-                tempGroup = []
+    # nums = [50]
+    for path in TRAIN_PATHS:
+        with open(path[0], "r") as f:
+            content = f.readlines()
+        content = [x.strip() for x in content]
+        allItems = []
+        for each in content:
+            splitedLine = re.split(r' +', each)
+            oneObj = {
+                "topic": splitedLine[0],
+                "uid": splitedLine[2],
+                "score": float(splitedLine[4])
+            }
+            allItems.append(oneObj)
+        topics = []
+        for item in allItems:
+            topics.append(item["topic"])
+        uniqueTopics = list(dict.fromkeys(topics))
+        uniqueTopics.sort()
+        groupedAll = []
+        for topic in uniqueTopics:
+            grouped = []
+            for t in allItems:
+                if t["topic"] == topic:
+                    grouped.append(t)
+                else:
+                    continue
+            if len(grouped) > 0:
+                groupedAll.append(grouped)
+        for g in groupedAll:
+            g.sort(key=lambda x: (x["score"], x["uid"]))
+        for member in groupedAll:
+            allMemberScores = []
+            for innerMember in member:
+                allMemberScores.append(float(innerMember["score"]))
+            memberMaxScore = max(allMemberScores)
+            memberMinScore = min(allMemberScores)
+            for innerMember in member:
+                if memberMaxScore != memberMinScore:
+                    innerMember["score"] = (float(innerMember["score"]) - memberMinScore) / (memberMaxScore - memberMinScore)
+                else:
+                    innerMember["score"] = 1.00
+        for each in groupedAll:
+            innerAllScores = []
+            for m in each:
+                innerAllScores.append(float(m["score"]))
+            innerMaxScore = max(innerAllScores)
+            for n in each:
+                n["score"] = innerMaxScore - n["score"]
+        for e in groupedAll:
+            e.sort(key=lambda y: (y["score"], y["uid"]))
+        cutoffGroupedAll = []
+        for num in nums:
+            num = float(num)
+            for c in groupedAll:
+                eachCutoffTotalScore = 0.0
                 for ele in c:
-                    if tempScore < eachCutoffScore:
-                        tempScore += float(ele["score"])
-                        tempGroup.append(ele)
-                cutoffGroupedAll.append(tempGroup)
-        for k in cutoffGroupedAll:
-            k.sort(key=lambda o: (o["score"], o["uid"]))
-        splitedPath = path.rsplit('/', 1)
-        outPath = splitedPath[0]
-        for e in cutoffGroupedAll:
-            for ind, innerEle in enumerate(e):
-                # Change the file name each run
-                f = open(outPath + "/" + "_" + str(int(num)), "a+")
-                # Change the last column according to the run
-                f.write(innerEle["topic"] + "    0    " + innerEle["uid"] + "    " + str(ind + 1) + "    " + str(innerEle["score"]) + "    " + "2017_umls_test\n")
-                f.close()
+                    eachCutoffTotalScore += ele["score"]
+                if eachCutoffTotalScore == 0.0:
+                    cutoffGroupedAll.append(c)
+                else:
+                    eachCutoffScore = eachCutoffTotalScore * (num / 100.0)
+                    tempScore = 0.0
+                    tempGroup = []
+                    for ele in c:
+                        if (tempScore + float(ele["score"])) <= eachCutoffScore:
+                            tempScore += float(ele["score"])
+                            tempGroup.append(ele)
+                        else:
+                            break
+                    cutoffGroupedAll.append(tempGroup)
+            for k in cutoffGroupedAll:
+                k.sort(key=lambda o: (o["score"], o["uid"]))
+            for e in cutoffGroupedAll:
+                for ind, innerEle in enumerate(e):
+                    desc = "{d}_{n}".format(d=path[2], n=int(num))
+                    line = "{topic} 0 {uid} {ind} {score} {desc}\n".format(topic=innerEle["topic"], uid=innerEle["uid"],
+                                                                           ind=ind + 1,
+                                                                           score=float(innerEle["score"]),
+                                                                           desc=desc)
+                    outPath = "{pathPrefix}_{num}.res".format(pathPrefix=path[1], num=int(num))
+                    f = open(outPath, "a+")
+                    f.write(line)
+                    f.close()
 
 
 if __name__ == '__main__':
